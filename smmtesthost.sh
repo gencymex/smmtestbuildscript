cdbe623aa438944a519bedcb5d98b44c82541a36
Alex Floyd <floyd@preossec.com>
2017-07-11 15:43:02 -0700

    changed the script to work with Fedora 26. Using fedora 26 provided QEMU binary, so the building QEMU source parts of the script have been taken out. OVMF Template is also pointing towards the fedora 26 iso.

--- smmtesthost.sh
+++ smmtesthost.sh
@@ -2,12 +2,9 @@
 #This script will install pre-requisites and enable secure remote connection, do not forget to grab you SSH keys to access later. Must have your Fedora 25 workstation ISO located in /var/lib/libvirt/images before running this script.
 
 
-#define script variables for QEMU and EDK2 first
+#define script variables for EDK2 first
 
 EDK2_SOURCE=$HOME/edk2
-QEMU_SOURCE=$HOME/qemu
-QEMU_BUILD=$HOME/qemu-build
-QEMU_INSTALL=/opt/qemu
 OVMF_INSTALL=/opt/edk2/share/ovmf-smm
 TEMPLATE_DIR=${pwd}
 
@@ -15,7 +12,6 @@ dnf -y group install with-optional virtualization
 
 dnf -y install xorg-x11-xauth pixman-devel spice-server-devel gcc-c++ nasm libuuid-devel acpica-tools patch python
 
-git clone git://git.qemu.org/qemu.git $QEMU_SOURCE
 git clone https://github.com/tianocore/edk2.git $EDK2_SOURCE
 git clone -b OpenSSL_1_1_0e https://github.com/openssl/openssl $EDK2_SOURCE/CryptoPkg/Library/OpensslLib/openssl
 
@@ -37,36 +33,6 @@ systemctl start libvirtd
 
 systemctl enable virtlogd
 systemctl start virtlogd
-
-
-#Check to see if QEMU is installed, if installed then remove, if not installed, then download and build
-
-#Build and install QEMU
-cd $QEMU_SOURCE
-git checkout a0def594286d
-mkdir -p -v $QEMU_BUILD
-cd $QEMU_BUILD
-
-$QEMU_SOURCE/configure \
-  --target-list=x86_64-softmmu,i386-softmmu \
-  --enable-spice \
-  --enable-trace-backends=log \
-  --enable-debug \
-  --prefix=$QEMU_INSTALL
-
-make -j $(getconf _NPROCESSORS_ONLN)
-make install
-
-
-#Now it is time to copy file attributes in order for libvirt to launch the QEMU binaries we just built
-
-for BINARY in $QEMU_INSTALL/bin/qemu-system-*; do
-  chown -c --reference=/usr/bin/qemu-system-x86_64 $BINARY
-  chmod -c --reference=/usr/bin/qemu-system-x86_64 $BINARY
-  chcon -v --reference=/usr/bin/qemu-system-x86_64 $BINARY
-done
-
-
 
 #now it is time to enable permanent nested virtualization
 
@@ -123,11 +89,11 @@ install -m 0644 -v \
   ${EDK2_SOURCE}/Build/OvmfIa32/NOOPT_GCC5/FV/OVMF_CODE.fd \
   ${OVMF_INSTALL}/OVMF_CODE.32.fd
 
-chcon -v --reference=${QEMU_INSTALL}/share/qemu/bios.bin ${OVMF_INSTALL}/*.fd
+chcon -v --reference=/usr/share/qemu/bios.bin ${OVMF_INSTALL}/*.fd
 
 
 
-#Assuming Fedora 25 workstation ISO located in /var/lib/libvirt/images
+#Assuming Fedora 26 workstation ISO located in /var/lib/libvirt/images
 
 qemu-img create -f qcow2 \
   -o compat=1.1 -o cluster_size=65536 \
